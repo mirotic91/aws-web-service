b9e765db2b92840f31700a795d51f4b2587dfbd9
박종욱 <jonguk1221@gmail.com>
2020-01-12 17:40:44 +0900

    init based posts

---
+++ src/main/java/me/mirotic/awswebservice/service/PostsService.java
@@ -0,0 +1,61 @@
+package me.mirotic.awswebservice.service;
+
+import lombok.RequiredArgsConstructor;
+import me.mirotic.awswebservice.domain.posts.Posts;
+import me.mirotic.awswebservice.domain.posts.PostsRepository;
+import me.mirotic.awswebservice.web.dto.PostsListResponseDto;
+import me.mirotic.awswebservice.web.dto.PostsResponseDto;
+import me.mirotic.awswebservice.web.dto.PostsSaveRequestDto;
+import me.mirotic.awswebservice.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        Posts posts = postsRepository.save(requestDto.toEntity());
+        return posts.getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return posts.getId();
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/me/mirotic/awswebservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package me.mirotic.awswebservice.web.dto;
+
+import lombok.Getter;
+import me.mirotic.awswebservice.domain.posts.Posts;
+
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
