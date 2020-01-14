495a0e8a497d22820045c6d50e6d6e2f9ea5e75b
박종욱 <jonguk1221@gmail.com>
2020-01-14 21:45:14 +0900

    add missing test

--- src/main/java/me/mirotic/awswebservice/web/IndexController.java
+++ src/main/java/me/mirotic/awswebservice/web/IndexController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 
@@ -19,8 +18,6 @@ import java.util.Objects;
 public class IndexController {
 
     private final PostsService postsService;
-
-    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
