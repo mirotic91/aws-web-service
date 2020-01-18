34e11aeeb35c6998b586ddc2fbbcd64dda070044
박종욱 <jonguk1221@gmail.com>
2020-01-18 21:46:41 +0900

    config session store jdbc

--- src/test/java/me/mirotic/awswebservice/web/HelloControllerTest.java
+++ src/test/java/me/mirotic/awswebservice/web/HelloControllerTest.java
@@ -1,9 +1,12 @@
 package me.mirotic.awswebservice.web;
 
+import me.mirotic.awswebservice.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +15,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(value = HelloController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    //
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
